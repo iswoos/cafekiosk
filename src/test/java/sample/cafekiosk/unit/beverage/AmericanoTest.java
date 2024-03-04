f49d10d51661077c4475e67a086f28764ece0bcc
seungwoo-lee <vugil@naver.com>
2024-03-05 00:26:22 +0900

    Feature : 카페 키오스크 관련 테스트코드 작성

---
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -0,0 +1,27 @@
+package sample.cafekiosk.unit.beverage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmericanoTest {
+
+    @Test
+    void getName() {
+        Americano americano = new Americano();
+
+//        Junit의 assert문
+//        assertEquals(americano.getName(), "아메리카노");
+
+//        assertj의 assertThat문
+        assertThat(americano.getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void getPrice() {
+        Americano americano = new Americano();
+
+        assertThat(americano.getPrice()).isEqualTo(4000);
+    }
+}
