217c41364f25a4c6ba1a34934baf25b1c9182f45
이승우 <swlee@joara.com>
2024-03-16 13:33:16 +0900

    Feature: Business Layer테스트 3

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.api.service.order;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,8 @@ import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.Stock;
+import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,9 +23,10 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
-import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
 
 @SpringBootTest
+@Transactional
 @ActiveProfiles("test")
 class OrderServiceTest {
 
@@ -38,13 +42,16 @@ class OrderServiceTest {
     @Autowired
     private OrderProductRepository orderProductRepository;
 
-    @AfterEach
-    void tearDown() {
-//        productRepository.deleteAll();  -- 두개의 차이가 있음. deleteAllInBatch를 강사는 더 선호하나, 아직 이유는 나오지 않음
-        orderProductRepository.deleteAllInBatch();
-        productRepository.deleteAllInBatch();
-        orderRepository.deleteAllInBatch();
-    }
+    @Autowired
+    private StockRepository stockRepository;
+
+//    @AfterEach
+//    void tearDown() {
+////        productRepository.deleteAll();  -- 두개의 차이가 있음. deleteAllInBatch를 강사는 더 선호하나, 아직 이유는 나오지 않음
+//        orderProductRepository.deleteAllInBatch();
+//        productRepository.deleteAllInBatch();
+//        orderRepository.deleteAllInBatch();
+//    }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
@@ -76,6 +83,52 @@ class OrderServiceTest {
                         tuple("002", 3000)
                 );
 
+    }
+
+    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrderWithStock() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime,10000);
+        assertThat(orderResponse.getProducts()).hasSize(4)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000),
+                        tuple("002", 3000),
+                        tuple("003", 5000)
+                );
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
+                );
     }
 
     @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다")
---
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockRepositoryTest.java
@@ -0,0 +1,45 @@
+package sample.cafekiosk.spring.domain.stock;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import sample.cafekiosk.spring.domain.product.Product;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.HOLD;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+
+@DataJpaTest
+class StockRepositoryTest {
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @DisplayName("상품번호 리스트로 재고를 조회한다")
+    @Test
+    void findAllByProductNumberIn() {
+        // given
+        Stock stock1 = Stock.create("001", 1);
+        Stock stock2 = Stock.create("002", 2);
+        Stock stock3 = Stock.create("003", 3);
+        stockRepository.saveAll(List.of(stock1, stock2, stock3));
+
+        // when
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(List.of("001", "002"));
+
+        // then
+        assertThat(stocks).hasSize(2)
+                // 검증하고자 하는 필드만 추출할 수 있는 기능
+                .extracting("productNumber", "quantity")
+                // 순서상관없이 포함하고 있는지 검증
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1),
+                        tuple("002", 2)
+                );
+    }
+
+}
